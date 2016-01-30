22faa8334458a5c6ba1b0f5fb6842ced79226fdc
NinePinNoTap <1100484@live.abertay.ac.uk>
2016-01-30 13:54:20 +0000

    Basic splash screen. Needs work once project progresses.

---
+++ Assets/Scripts/ScrollBackground.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScrollBackground : MonoBehaviour
+{
+	public float moveSpeed = 0.5f;
+
+	private Image BackgroundImg;
+
+	private float StartPos;
+	private float MinX;
+
+	// Use this for initialization
+	void Start ()
+	{
+		BackgroundImg = this.GetComponent<Image>();
+
+		StartPos = BackgroundImg.transform.position.x;
+		MinX = StartPos - BackgroundImg.sprite.rect.width / 2;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 Pos = BackgroundImg.transform.position;
+		Pos -= new Vector3(moveSpeed, 0, 0);
+		if(Pos.x < MinX)
+			Pos.x = StartPos;
+
+		BackgroundImg.transform.position = Pos;
+	}
+}
--- Assets/Scripts/Wall_Collision.cs
+++ Assets/Scripts/Wall_Collision.cs
@@ -1,33 +1,35 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-public class Wall_Collision : MonoBehaviour {
-
+public class Wall_Collision : MonoBehaviour
+{
 	private RaycastHit hit;
 	private Ray ray;
 
 	enum direction {up, down, left, right};
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
 	}
 
-	void OnTriggerStay (Collider other) {
-		if (other.gameObject.tag = "wall") {
-			
-		
+	void OnTriggerStay (Collider other)
+	{
+		if (other.gameObject.tag == "wall")
+		{
 		}
 	}
 
-	void lookAtWall(direction x, Collider other) {
-
+	void lookAtWall(direction x, Collider other)
+	{
 		ray.origin = transform.position;
-		ray.direction = 
+		//ray.direction = 
 	}
 }
