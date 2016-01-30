9a019778747e84fcb2044fb0722691490fe0bf8e
unknown <1000264@MC10792.uad.ac.uk>
2016-01-30 21:56:36 +0000

    Ross's Level layouts and scene change

---
+++ Assets/Scripts/Scene_transition.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scene_transition : MonoBehaviour {
+
+	public void transition() {
+		Application.LoadLevel (Random.Range (1, 10));
+	}
+}
